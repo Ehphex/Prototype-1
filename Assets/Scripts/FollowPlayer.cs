36401832695277e7ddac7972aa49fbc35aa118ed
Tumaini Johnson <tujohnson@davidson.edu>
2026-09-02 12:46:25 -0400

    Initial commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player; // Reference to the player object
+    private Vector3 offset = new Vector3(0, 5, 7); // Offset between the camera and the player
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
