c9dba503d7582fdd1a6ca365000bce850b505b3a
Tumaini Johnson <tujohnson@davidson.edu>
2026-09-05 22:24:16 -0400

    commit

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player; // Reference to the player object
-    private Vector3 offset = new Vector3(0, 5, 7); // Offset between the camera and the player
+    private Vector3 offset = new Vector3(30, 0, 0); // Offset between the camera and the player
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Speed and turn speed of the vehicle
     public float speed = 5.0f; // Speed of the vehicle
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float turnSpeed;
+    // Input action for movement
+    public InputAction moveAction;
+    // Input value for movement
+    private Vector2 moveInput;
+
+    private void Start()
     {
-        
+        // Enable the move action
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Read the input value from the move action
+        moveInput = moveAction.ReadValue<Vector2>();
         //Move the Vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * moveInput.x);
     }
 }
